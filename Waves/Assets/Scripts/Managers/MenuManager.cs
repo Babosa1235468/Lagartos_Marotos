518900f25f7ebf22a5fa3fba3f84d0dd85192700
babosa <todribarbosa@gmail.com>
2026-02-24 10:35:47 +0000

    Menu
    a modificar o menu de começo para escolher o modo, costumização e isso tudo, se não a saavedra fica zangada

--- Waves/Assets/Scripts/Managers/MenuManager.cs
+++ Waves/Assets/Scripts/Managers/MenuManager.cs
@@ -4,9 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject FirstMenu;
     public void PlayGame()
     {
         SceneManager.LoadScene("Game_Damage", LoadSceneMode.Single);
     }
-
+    public void LoadChoosePlayerModeMenu()
+    {
+        Debug.Log("Tentaste esconder o menu");
+        FirstMenu.SetActive(false);
+    }
 }
