d6f6e9b5cd348d5b805f0284641955bf435e531f
babosa <todribarbosa@gmail.com>
2026-03-02 14:21:09 +0000

    Fixed bullet script and Player now spawns on top of ground

--- Waves/Assets/Scripts/BulletScripts/BulletHit.cs
+++ Waves/Assets/Scripts/BulletScripts/BulletHit.cs
@@ -7,13 +7,15 @@ public class BulletHit : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // ignore if hitting the shooter
-        if (collision.gameObject == owner) return;
+        if (collision.gameObject == owner)
+        {
+            return;
+        } 
 
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log(collision.GetComponent<PlayerMovement>().isInvincible);
             // se o player estiver invencivel, nao leva dano
-            if (collision.GetComponent<PlayerMovement>().isInvincible)
+            if (!collision.GetComponent<PlayerMovement>().isInvincible)
             {
                 collision.GetComponent<PlayerMovement>().Damage(dmg);
             }
