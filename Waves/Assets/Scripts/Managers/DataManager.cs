6c93df6da0d583ea6838a6bcbc588539921e19f3
babosa <todribarbosa@gmail.com>
2026-03-09 13:58:59 +0000

    working on map changing and menu

--- Waves/Assets/Scripts/Managers/DataManager.cs
+++ Waves/Assets/Scripts/Managers/DataManager.cs
@@ -3,6 +3,18 @@ using UnityEngine;
 public class DataManager : MonoBehaviour
 {
     public static DataManager instance;
+    [Header("Mapa Escolhido")]
+    public GameObject MapaEscolhido;
+    [Header("Cores dos Players")]
+    public Color P1SpriteColor;
+    public Color P2SpriteColor;
+    [Header("Player 1 Controls")]
+    public KeyCode[] P1Controls;
+    [Header("Player 2 Controls")]
+    public KeyCode[] P2Controls;
+    [Header("Definicoes de Jogo")]
+    public bool UsingPowerUps = true;
+
     void Awake()
     {
         //criar isto estático
@@ -10,5 +22,9 @@ public class DataManager : MonoBehaviour
 
         // meter para nao destruir entre cenas
         DontDestroyOnLoad(gameObject);
+    }
+    public void ChangeMap(GameObject Mapa)
+    {
+        MapaEscolhido = Mapa;
     }
 }
