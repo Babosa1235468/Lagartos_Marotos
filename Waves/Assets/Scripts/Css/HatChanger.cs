33d812ad50cad11f8ef7ccf63bbbf905a8dbef68
babosa <todribarbosa@gmail.com>
2026-03-22 21:54:46 +0000

    added hats

--- Waves/Assets/Scripts/Css/HatChanger.cs
+++ Waves/Assets/Scripts/Css/HatChanger.cs
@@ -5,15 +5,28 @@ public class HatChanger : MonoBehaviour
     public void changeHat(int index)
     {
         Sprite hat = gameObject.GetComponentInChildren<SpriteRenderer>().sprite;
-        if(index == 1)
+
+        if (index == 1)
         {
+            if (DataManager.instance.P1Chapeu == hat)
+            {
+                GameObject.FindGameObjectWithTag("Player1").transform.Find("Sprites/Hat").GetComponent<SpriteRenderer>().sprite = null;
+                DataManager.instance.P1Chapeu = null;
+                return;
+            }
             GameObject.FindGameObjectWithTag("Player1").transform.Find("Sprites/Hat").GetComponent<SpriteRenderer>().sprite = hat;
-            DataManager.instance.P1Chapeu = hat; 
+            DataManager.instance.P1Chapeu = hat;
         }
         else
         {
+            if (DataManager.instance.P2Chapeu == hat)
+            {
+                GameObject.FindGameObjectWithTag("Player2").transform.Find("Sprites/Hat").GetComponent<SpriteRenderer>().sprite = null;
+                DataManager.instance.P2Chapeu = null;
+                return;
+            }
             GameObject.FindGameObjectWithTag("Player2").transform.Find("Sprites/Hat").GetComponent<SpriteRenderer>().sprite = hat;
-            DataManager.instance.P2Chapeu = hat; 
+            DataManager.instance.P2Chapeu = hat;
         }
     }
 }
